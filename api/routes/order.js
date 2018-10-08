a4d9ff6d93fb3b4c6b23ae4e1472a689afbf9897
Hamzakhann <hamzakhann66@gmail.com>
2018-10-09 01:15:37 +0500

    populate some data

--- api/routes/order.js
+++ api/routes/order.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Order = require('../models/order');
+const Product = require('../models/product')
 
 
 
 router.get('/' , (req , res , next) =>{
     Order.find()
     .select('product quantity _id')
+    .populate('product' , 'name')
     .exec()
     .then(docs =>{
         res.status.json(docs)
@@ -20,40 +22,50 @@ router.get('/' , (req , res , next) =>{
 
 
 router.post('/' , (req , res , next) =>{
-    const order = new Order({
-        _id : mongoose.Types.ObjectId(),
-        quantity : req.body.quantity,
-        product  : req.body.productId
-    });
-    order
-    .save()
+    Product.findById(req.body.productId)
+    .then(product =>{
+        if(!product){
+            return res.status(404).json({message : 'product not found'})
+        }
+        const order = new Order({
+            _id : mongoose.Types.ObjectId(),
+            quantity : req.body.quantity,
+            product  : req.body.productId
+        });
+        return order.save();
+    })
     .then(result =>{
         res.status(201).json(result)
+    })
+    .catch(err =>{
+        res.status(500).json({message : 'Product not found'})
+    })
+    
+})
+
+
+router.get('/:orderId' , (req , res , next) =>{
+    Order.findById(req.params.orderId)
+    .exec()
+    .then(order =>{
+        if(!order){
+            return res.status(500).json({message : 'order not found'})
+        }
+        res.status(200).json(order)
     })
     .catch(err =>{
         res.status(500).json({error : err})
     })
 })
 
-
-router.get('/:orderId' , (req , res , next) =>{
-    const id = req.params.orderId;
-    if(id === 'special'){
-        res.status(200).json({
-            message : 'you order pass id',
-            id : id
-        });
-    }else {
-        res.status.json({
-            message : 'It is order product',
-            id : id
-        })
-    }
-})
-
 router.delete('/:orderId' , (req , res , next) =>{
-    res.status(200).json({
-        message : 'delete order works'
+    Order.remove({_id : req.params.orderId})
+    .exec()
+    .then(result =>{
+        res.status(200).json({message : 'order deleted'})
+    })
+    .catch(err =>{
+        res.status(500).json({error: err})
     })
 })
 
